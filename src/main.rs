7cdf9e9c55ef4f3a9faa7b757e257597e6f60be1
Rehan Rana <rehanalirana@tuta.io>
2020-07-27 20:16:08 -0400

    clippy and cargo fmt

--- src/main.rs
+++ src/main.rs
@@ -1,42 +1,49 @@
-use notify::{RecommendedWatcher, Watcher, RecursiveMode, DebouncedEvent};
-use std::sync::mpsc::channel;
-use std::time::Duration;
-use std::error::Error;
 use clap::Clap;
+use notify::{DebouncedEvent, RecommendedWatcher, RecursiveMode, Watcher};
+use std::error::Error;
 use std::process::Command;
+use std::sync::mpsc::channel;
 use std::thread::sleep;
+use std::time::Duration;
 
 #[derive(Clap)]
 #[clap(about = "Updates efibootstub when linux kernel is updated")]
 struct Args {
-    #[clap(short, long, value_name = "COMMAND", about = "Specify command to be run when linux kernel is updated. Place !VERSION where kernel version should be", required = true)]
+    #[clap(
+        short,
+        long,
+        value_name = "COMMAND",
+        about = "Specify command to be run when linux kernel is updated. Place !VERSION where kernel version should be",
+        required = true
+    )]
     command: String,
 
-    #[clap(short, long, value_name = "NUM", about = "Specify bootnum of entry to be replaced", required = true)]
+    #[clap(
+        short,
+        long,
+        value_name = "NUM",
+        about = "Specify bootnum of entry to be replaced",
+        required = true
+    )]
     bootnum: String,
 }
 
 fn run_command(vnum: &str, args: &Args) -> Result<(), Box<dyn Error>> {
-    let mut command_split = args.command.split("'");
-    let mut block_mod = String::from("");
+    let command_split = args.command.split("'");
     let mut rm_handle = Command::new("efibootmgr");
     let mut create_handle = Command::new("");
     let mut first_run = true;
     let mut sing_quote_switch = false;
-    rm_handle.arg("-b")
-        .arg(&args.bootnum)
-        .arg("-B");
+    rm_handle.arg("-b").arg(&args.bootnum).arg("-B");
     for block in command_split {
         if sing_quote_switch {
             create_handle.arg(block.replace("!VERSION", vnum));
-        }
-        else {
+        } else {
             for word in block.split_whitespace() {
                 if first_run {
                     first_run = false;
                     create_handle = Command::new(word);
-                }
-                else {
+                } else {
                     create_handle.arg(word.replace("!VERSION", vnum));
                 }
             }
@@ -46,7 +53,7 @@ fn run_command(vnum: &str, args: &Args) -> Result<(), Box<dyn Error>> {
 
     rm_handle.spawn().unwrap();
     sleep(Duration::from_secs(1));
-    println!("COMMAND = {:?}", create_handle); 
+    println!("COMMAND = {:?}", create_handle);
     create_handle.spawn().unwrap();
     Ok(())
 }
@@ -66,20 +73,17 @@ fn watch(args: Args) -> notify::Result<()> {
     // This is a simple loop, but you may want to use more complex logic here,
     // for example to handle I/O.
     loop {
-        let mut vnum: &str = "";
+        let vnum: &str;
         match rx.recv() {
             Ok(event) => {
                 if let DebouncedEvent::Create(path) = event {
                     let file_name = path.file_name().unwrap();
-                    let file_name = path.to_str().unwrap();
+                    let file_name = file_name.to_str().unwrap();
                     if file_name.contains("vmlinuz") {
                         vnum = &file_name[8..];
                         run_command(vnum, &args).unwrap();
-
                     }
-                        
                 }
-
             }
             Err(e) => println!("{:?}", e),
         }
